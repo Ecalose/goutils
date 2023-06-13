4b769f82a2db9c66ec6ce801cfbbff8e66f174a9
Icarus <icarus@example.com>
2023-06-13 21:36:12 +0800

    update

---
+++ random/random.go
@@ -0,0 +1,38 @@
+package random
+
+import "math/rand"
+
+func Num(n int) string {
+	charset := "0123456789"
+	num := make([]byte, n)
+	for i := range num {
+		num[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(num)
+}
+func Letter(n int) string {
+	charset := "abcdefghijklmnopqrstuvwxyz"
+	letter := make([]byte, n)
+	for i := range letter {
+		letter[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(letter)
+}
+
+func Pwd(n int) string {
+	charset := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789()*&#?+-"
+	letter := make([]byte, n)
+	for i := range letter {
+		letter[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(letter)
+}
+func RanPwd(min, max int) string {
+	n := rand.Intn(max-min) + min
+	charset := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789()*&#?+-"
+	letter := make([]byte, n)
+	for i := range letter {
+		letter[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(letter)
+}
